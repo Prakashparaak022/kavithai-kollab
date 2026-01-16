629d7e1ad2ff6b5d4684d05b5a31f1f1714cd9d2
Prakash <prakash.muthusamy@oxfyn.com>
2026-01-16 12:14:46 +0530

    fix: padding

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -4,7 +4,7 @@ import Header from "./header";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Providers>
-      <div className="min-h-screen flex justify-center p-6">
+      <div className="min-h-screen flex justify-center p-2 lg:p-6">
         <div className="w-full rounded-2xl shadow-xl overflow-hidden relative">
           <Header />
           {children}
